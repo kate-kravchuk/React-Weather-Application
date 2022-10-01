c5e65e74283e22bd1dfd61f0a83c15832cd0f986
kate-kravchuk <106491612+kate-kravchuk@users.noreply.github.com>
2022-10-01 18:59:52 +0200

    Added weather blocks

--- src/Weather.js
+++ src/Weather.js
@@ -4,6 +4,20 @@ import "./Weather.css";
 export default function Weather() {
   return (
     <div className="Weather">
+      <form>
+        <div className="row">
+          <div className="col-9">
+            <input
+              type="search"
+              placeholder="Search for a city"
+              className="form-control"
+            />
+          </div>
+          <div className="col-3">
+            <input type="submit" value="Search" className="btn btn-primary" />
+          </div>
+        </div>
+      </form>
       <h1>New York</h1>
       <ul>
         <li>Saturday, 7:00</li>
@@ -17,7 +31,13 @@ export default function Weather() {
           ></img>
           6°C
         </div>
-        <div className="col-6"></div>
+        <div className="col-6">
+          <ul>
+            <li>Precipitation: 15%</li>
+            <li>Wind: 20km/h</li>
+            <li>Humidity: 20%</li>
+          </ul>
+        </div>
       </div>
     </div>
   );
